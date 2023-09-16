93ac42fdd7fa17d5b240d585ae8bfc2be574ebb8
Juan Camilo Sánchez <juan.sanchez53@udea.edu.co>
2023-09-15 22:13:11 -0500

    Proyect-Creation

---
+++ src/components/Cards.jsx
@@ -0,0 +1,8 @@
+
+const Cards = () => { //array con descripción de todas las cards, nombre, source, caracteristicas, titulo, estudiar chakra
+  return (
+    <div>Cards</div>
+  )
+}
+
+export default Cards
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Cards from "../components/Cards"
+
+const Home = () => {
+  return (
+    <div>
+        {
+            //aqui vamos a invocar todo los otros componentes que integran el home
+        }
+        <Cards />
+    </div>
+  )
+}
+
+export default Home
