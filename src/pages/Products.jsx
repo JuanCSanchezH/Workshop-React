81e84bd885276b268ede001c6c960735e88f7bdf
Juan Camilo Sánchez <juan.sanchez53@udea.edu.co>
2023-09-18 21:34:30 -0500

    finished-navbar

---
+++ src/pages/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>Estamos en Products</div>
+  )
+}
+
+export default Products
---
+++ src/pages/Support.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Support = () => {
+  return (
+    <div>Estamos en Support</div>
+  )
+}
+
+export default Support
