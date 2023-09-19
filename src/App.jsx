ff280d03696f14577310264661a637fed1a7b2ab
Nathalia Acosta Vélez <nathalia285@gmail.com>
2023-09-18 20:46:05 -0500

    LastVersion

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from './pages/Home'
+import Content from './components/Content'
+import './styles/App.scss'
+
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home/>} />
-        <Route path="/home" element={<Home/>} />
-      </Routes>
-    </BrowserRouter>
+    <div className='App'>
+      <Content />
+    </div>
   )
 }
 
